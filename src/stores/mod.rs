94c149e3d350019135043d3a1e60710b6a2204fd
Brendon Felix <brendon.felix.c@gmail.com>
2026-02-25 14:43:56 -0700

    chore: remove old src directory and outdated files

--- src/stores/mod.rs
+++
@@ -1,9 +0,0 @@
-// pub mod task_store;
-// pub mod ui_store;
-pub mod database_store;
-pub mod drag_drop_store;
-
-// pub use task_store::TaskStore;
-// pub use ui_store::UiStateStore;
-pub use database_store::DatabaseStore;
-pub use drag_drop_store::DragDropStore;
